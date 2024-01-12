809b94f8dff0ede83b8c3ed77004579e2820cbd2
morten <morten.c.nissov@gmail.com>
2024-01-12 18:06:43 +0100

    combingin into one module

---
+++ src/modules/mc_sm_control/mc_sm_control.cpp
@@ -0,0 +1,369 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2013-2018 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file mc_sm_att_control.cpp
+ * Multicopter attitude controller.
+ *
+ * @author Lorenz Meier		<lorenz@px4.io>
+ * @author Anton Babushkin	<anton.babushkin@me.com>
+ * @author Sander Smeets	<sander@droneslab.com>
+ * @author Matthias Grob	<maetugr@gmail.com>
+ * @author Beat Küng		<beat-kueng@gmx.net>
+ *
+ */
+
+#include "mc_sm_control.hpp"
+
+#include <drivers/drv_hrt.h>
+#include <mathlib/math/Limits.hpp>
+#include <mathlib/math/Functions.hpp>
+
+using namespace matrix;
+
+MulticopterSMControl::MulticopterSMControl(bool vtol) :
+	ModuleParams(nullptr),
+	WorkItem(MODULE_NAME, px4::wq_configurations::nav_and_controllers),
+	// _vehicle_attitude_setpoint_pub(vtol ? ORB_ID(mc_virtual_attitude_setpoint) : ORB_ID(vehicle_attitude_setpoint)),
+	_loop_perf(perf_alloc(PC_ELAPSED, MODULE_NAME": cycle"))
+{
+	parameters_updated();
+	// Rate of change 5% per second -> 1.6 seconds to ramp to default 8% MPC_MANTHR_MIN
+	//_manual_throttle_minimum.setSlewRate(0.05f);
+	// Rate of change 50% per second -> 2 seconds to ramp to 100%
+	//_manual_throttle_maximum.setSlewRate(0.5f);
+}
+
+MulticopterSMControl::~MulticopterSMControl()
+{
+	perf_free(_loop_perf);
+}
+
+bool
+MulticopterSMControl::init()
+{
+	if (!_vehicle_local_position_sub.registerCallback()) {
+		PX4_ERR("callback registration failed");
+		return false;
+	}
+
+	return true;
+}
+
+void
+MulticopterSMControl::parameters_updated()
+{
+	// Store some of the parameters in a more convenient way & precompute often-used values
+	const float posLambda3x3[] = {
+		_param_pos_lam_x.get(), 0, 0,
+		0, _param_pos_lam_y.get(), 0,
+		0, 0, _param_pos_lam_z.get()
+	};
+	_position_control.setLambda(matrix::Matrix3f(posLambda3x3));
+
+	const float posGain3x3[] = {
+		_param_pos_gain_x.get(), 0, 0,
+		0, _param_pos_gain_y.get(), 0,
+		0, 0, _param_pos_gain_z.get()
+	};
+  _position_control.setSwitchingGain(matrix::Matrix3f(posGain3x3));
+
+	const float attLambda3x3[] = {
+		_param_att_lam_x.get(), 0, 0,
+		0, _param_att_lam_y.get(), 0,
+		0, 0, _param_att_lam_z.get()
+	};
+	_attitude_control.setLambda(matrix::Matrix3f(attLambda3x3));
+
+	const float attGain3x3[] = {
+		_param_att_gain_x.get(), 0, 0,
+		0, _param_att_gain_y.get(), 0,
+		0, 0, _param_att_gain_z.get()
+	};
+  _attitude_control.setSwitchingGain(matrix::Matrix3f(attGain3x3));
+
+	_position_control.setTanhFactor(_param_pos_tanh_factor.get());
+	_attitude_control.setTanhFactor(_param_att_tanh_factor.get());
+	_position_control.setMass(_param_mass.get());
+}
+
+void
+MulticopterSMControl::Run()
+{
+	if (should_exit()) {
+		_vehicle_local_position_sub.unregisterCallback();
+		exit_and_cleanup();
+		return;
+	}
+
+	perf_begin(_loop_perf);
+
+	// Check if parameters have changed
+	if (_parameter_update_sub.updated()) {
+		// clear update
+		parameter_update_s param_update;
+		_parameter_update_sub.copy(&param_update);
+
+		updateParams();
+		parameters_updated();
+	}
+
+	// run controller on position updates
+	vehicle_local_position_s vehicle_local_position;
+
+	if (_vehicle_local_position_sub.update(&vehicle_local_position)) {
+
+		// Guard against too small (< 0.2ms) and too large (> 20ms) dt's.
+		//const float dt = math::constrain(((vehicle_attitude.timestamp_sample - _last_run) * 1e-6f), 0.0002f, 0.02f);
+		_last_run = vehicle_local_position.timestamp_sample;
+
+		_position_control.setPosition(Vector3f(vehicle_local_position.x, vehicle_local_position.y, vehicle_local_position.z));
+		_position_control.setLinearVelocity(Vector3f(vehicle_local_position.vx, vehicle_local_position.vy, vehicle_local_position.vz));
+		_position_control.setLinearAcceleration(Vector3f(vehicle_local_position.ax, vehicle_local_position.ay, vehicle_local_position.az));
+
+		// Check for new position setpoint
+		if (_vehicle_local_position_setpoint_sub.updated()) {
+			vehicle_local_position_setpoint_s vehicle_local_position_setpoint;
+
+			if (_vehicle_local_position_setpoint_sub.copy(&vehicle_local_position_setpoint)
+			    && (vehicle_local_position_setpoint.timestamp > _last_vehicle_local_position_setpoint)) {
+
+				_position_control.setPositionSetpoint(Vector3f(vehicle_local_position_setpoint.x, vehicle_local_position_setpoint.y, vehicle_local_position_setpoint.z));
+				_position_control.setLinearVelocitySetpoint(Vector3f(vehicle_local_position_setpoint.vx, vehicle_local_position_setpoint.vy, vehicle_local_position_setpoint.vz));
+				_position_control.setLinearAccelerationSetpoint(Vector3f(vehicle_local_position_setpoint.acceleration[0], vehicle_local_position_setpoint.acceleration[1], vehicle_local_position_setpoint.acceleration[2]));
+				_position_control.setYawSetpoint(vehicle_local_position_setpoint.yaw);
+				_last_vehicle_local_position_setpoint = vehicle_local_position_setpoint.timestamp;
+			}
+		}
+
+
+		// update angular velocity
+		// update angular acceleration
+		if (_vehicle_attitude_sub.updated()) {
+			vehicle_attitude_s vehicle_attitude;
+			if(_vehicle_attitude_sub.copy(&vehicle_attitude)) {
+				const Quatf q(vehicle_attitude.q);
+				_position_control.setAttitude(q);
+				_attitude_control.setAttitude(q);
+			}
+		}
+
+		if (_vehicle_angular_velocity_sub.updated()) {
+			vehicle_angular_velocity_s vehicle_angular_velocity;
+			if(_vehicle_angular_velocity_sub.copy(&vehicle_angular_velocity)) {
+				_attitude_control.setAngularVelocity(Vector3f(vehicle_angular_velocity.xyz));
+				_attitude_control.setAngularAcceleration(Vector3f(vehicle_angular_velocity.xyz_derivative));
+			}
+		}
+
+		/* check for updates in other topics */
+		//_manual_control_setpoint_sub.update(&_manual_control_setpoint);
+		if (_vehicle_control_mode_sub.updated()) {
+			if (_vehicle_control_mode_sub.copy(&_vehicle_control_mode)) {
+				/* PX4_INFO("%lu", _vehicle_control_mode.timestamp);
+				PX4_INFO("offboard %d", _vehicle_control_mode.flag_control_offboard_enabled);
+				PX4_INFO("manual %d", _vehicle_control_mode.flag_control_manual_enabled);
+				PX4_INFO("position %d", _vehicle_control_mode.flag_control_position_enabled);
+				PX4_INFO("velocity %d", _vehicle_control_mode.flag_control_velocity_enabled);
+				PX4_INFO("altitude %d", _vehicle_control_mode.flag_control_altitude_enabled);
+				PX4_INFO("climb rate %d", _vehicle_control_mode.flag_control_climb_rate_enabled);
+				PX4_INFO("accerl %d", _vehicle_control_mode.flag_control_acceleration_enabled);
+				PX4_INFO("attitude %d", _vehicle_control_mode.flag_control_attitude_enabled);
+				PX4_INFO("rates %d", _vehicle_control_mode.flag_control_rates_enabled); */
+			}
+		}
+
+		if (_vehicle_status_sub.updated()) {
+			vehicle_status_s vehicle_status;
+
+			if (_vehicle_status_sub.copy(&vehicle_status)) {
+
+				const bool armed = (vehicle_status.arming_state == vehicle_status_s::ARMING_STATE_ARMED);
+				// _spooled_up = armed && hrt_elapsed_time(&vehicle_status.armed_time) > _param_com_spoolup_time.get() * 1_s;
+				_spooled_up = armed;
+			}
+		}
+
+		// =================================
+		// publish offboard control commands
+		// =================================
+		offboard_control_mode_s ocm{};
+		ocm.position = false;
+		ocm.velocity = false;
+		ocm.acceleration = false;
+		ocm.attitude = true;
+		ocm.body_rate = false;
+		ocm.thrust_and_torque = true;
+		ocm.direct_actuator = false;
+		ocm.timestamp = hrt_absolute_time();
+		_offboard_control_mode_pub.publish(ocm);
+
+
+
+		//compute control torques and thrust
+		if (true) {
+
+			// if (_vehicle_control_mode.flag_control_offboard_enabled) {
+
+			// 	generate_attitude_setpoint(q, dt, _reset_yaw_sp);
+			// 	attitude_setpoint_generated = true;
+
+			// } else {
+			// 	_man_roll_input_filter.reset(0.f);
+			// 	_man_pitch_input_filter.reset(0.f);
+			// }
+
+			if (_vehicle_control_mode.flag_control_offboard_enabled) {
+				_position_control.setPositionSetpoint(Vector3f(0.0f,0.0f,-2.5f));
+				_position_control.setLinearVelocitySetpoint(Vector3f(0.0f,0.0f,0.0f));
+				_position_control.setLinearAccelerationSetpoint(Vector3f(0.0f,0.0f,0.0f));
+
+				float thrust_setpoint;
+				Quatf attitude_setpoint;
+				_position_control.update(thrust_setpoint, attitude_setpoint);
+
+				_attitude_control.setAttitudeSetpoint(attitude_setpoint);
+				Vector3f torque_setpoint = _attitude_control.update();
+
+				// publish thrust and attitude setpoints
+				vehicle_thrust_setpoint_s vehicle_thrust_setpoint{};
+				vehicle_torque_setpoint_s vehicle_torque_setpoint{};
+
+				// _thrust_setpoint_body.copyTo(vehicle_thrust_setpoint.xyz);
+				vehicle_torque_setpoint.xyz[0] = PX4_ISFINITE(torque_setpoint(0)) ? torque_setpoint(0) : 0.f;
+				vehicle_torque_setpoint.xyz[1] = PX4_ISFINITE(torque_setpoint(1)) ? torque_setpoint(1) : 0.f;
+				vehicle_torque_setpoint.xyz[2] = PX4_ISFINITE(torque_setpoint(2)) ? torque_setpoint(2) : 0.f;
+
+				for(int i=0; i<3; i++){
+					torque_setpoint(i) = constrain(torque_setpoint(i),-1.f,1.f);
+				}
+				PX4_INFO("thrust setpoint: %f", (double)thrust_setpoint);
+				thrust_setpoint = -constrain(thrust_setpoint, 0.0f, _param_thrust_max.get()) / _param_thrust_max.get();
+				PX4_INFO("thrust setpoint (normalized): %f", (double)thrust_setpoint);
+
+				vehicle_thrust_setpoint.timestamp_sample = vehicle_local_position.timestamp_sample;
+				vehicle_thrust_setpoint.timestamp = hrt_absolute_time();
+				vehicle_thrust_setpoint.xyz[0] = 0.0f;
+				vehicle_thrust_setpoint.xyz[1] = 0.0f;
+				vehicle_thrust_setpoint.xyz[2] = thrust_setpoint;
+				_vehicle_thrust_setpoint_pub.publish(vehicle_thrust_setpoint);
+
+				vehicle_torque_setpoint.timestamp_sample = vehicle_local_position.timestamp_sample;
+				vehicle_torque_setpoint.timestamp = hrt_absolute_time();
+				_vehicle_torque_setpoint_pub.publish(vehicle_torque_setpoint);
+			}
+
+		}
+
+
+
+	}
+
+	perf_end(_loop_perf);
+}
+
+int MulticopterSMControl::task_spawn(int argc, char *argv[])
+{
+	bool vtol = false;
+
+	if (argc > 1) {
+		if (strcmp(argv[1], "vtol") == 0) {
+			vtol = true;
+		}
+	}
+
+	MulticopterSMControl *instance = new MulticopterSMControl(vtol);
+
+	if (instance) {
+		_object.store(instance);
+		_task_id = task_id_is_work_queue;
+
+		if (instance->init()) {
+			return PX4_OK;
+		}
+
+	} else {
+		PX4_ERR("alloc failed");
+	}
+
+	delete instance;
+	_object.store(nullptr);
+	_task_id = -1;
+
+	return PX4_ERROR;
+}
+
+int MulticopterSMControl::custom_command(int argc, char *argv[])
+{
+	return print_usage("unknown command");
+}
+
+int MulticopterSMControl::print_usage(const char *reason)
+{
+	if (reason) {
+		PX4_WARN("%s\n", reason);
+	}
+
+	PRINT_MODULE_DESCRIPTION(
+		R"DESCR_STR(
+### Description
+This implements the multicopter attitude controller. It takes attitude
+setpoints (`vehicle_attitude_setpoint`) as inputs and outputs a rate setpoint.
+
+The controller has a P loop for angular error
+
+Publication documenting the implemented Quaternion Attitude Control:
+Nonlinear Quadrocopter Attitude Control (2013)
+by Dario Brescianini, Markus Hehn and Raffaello D'Andrea
+Institute for Dynamic Systems and Control (IDSC), ETH Zurich
+
+https://www.research-collection.ethz.ch/bitstream/handle/20.500.11850/154099/eth-7387-01.pdf
+
+)DESCR_STR");
+
+	PRINT_MODULE_USAGE_NAME("mc_sm_control", "controller");
+	PRINT_MODULE_USAGE_COMMAND("start");
+	PRINT_MODULE_USAGE_ARG("vtol", "VTOL mode", true);
+	PRINT_MODULE_USAGE_DEFAULT_COMMANDS();
+
+	return 0;
+}
+
+
+/**
+ * Multicopter attitude control app start / stop handling function
+ */
+extern "C" __EXPORT int mc_sm_control_main(int argc, char *argv[])
+{
+	return MulticopterSMControl::main(argc, argv);
+}
---
+++ src/modules/mc_sm_control/mc_sm_control.hpp
@@ -0,0 +1,158 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2013-2019 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+#pragma once
+
+#include <matrix/matrix/math.hpp>
+#include <perf/perf_counter.h>
+#include <px4_platform_common/px4_config.h>
+#include <px4_platform_common/defines.h>
+#include <px4_platform_common/module.h>
+#include <px4_platform_common/module_params.h>
+#include <px4_platform_common/posix.h>
+#include <px4_platform_common/px4_work_queue/WorkItem.hpp>
+#include <uORB/Publication.hpp>
+#include <uORB/Subscription.hpp>
+#include <uORB/SubscriptionCallback.hpp>
+#include <uORB/topics/manual_control_setpoint.h>
+#include <uORB/topics/parameter_update.h>
+#include <uORB/topics/vehicle_attitude.h>
+#include <uORB/topics/vehicle_attitude_setpoint.h>
+#include <uORB/topics/vehicle_control_mode.h>
+#include <uORB/topics/vehicle_land_detected.h>
+#include <uORB/topics/vehicle_local_position.h>
+#include <uORB/topics/vehicle_local_position_setpoint.h>
+#include <uORB/topics/vehicle_thrust_setpoint.h>
+#include <uORB/topics/vehicle_torque_setpoint.h>
+#include <uORB/topics/vehicle_thrust_setpoint.h>
+#include <uORB/topics/vehicle_angular_velocity.h>
+
+#include <uORB/topics/vehicle_status.h>
+#include <lib/mathlib/math/filter/AlphaFilter.hpp>
+#include <lib/slew_rate/SlewRate.hpp>
+#include <uORB/topics/offboard_control_mode.h>
+
+#include <SMPositionControl.hpp>
+#include <SMAttitudeControl.hpp>
+
+using namespace time_literals;
+
+class MulticopterSMControl : public ModuleBase<MulticopterSMControl>, public ModuleParams,
+	public px4::WorkItem
+{
+public:
+	MulticopterSMControl(bool vtol = false);
+	~MulticopterSMControl() override;
+
+	/** @see ModuleBase */
+	static int task_spawn(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int custom_command(int argc, char *argv[]);
+
+	/** @see ModuleBase */
+	static int print_usage(const char *reason = nullptr);
+
+	bool init();
+
+private:
+	void Run() override;
+
+	/**
+	 * initialize some vectors/matrices from parameters
+	 */
+	void parameters_updated();
+
+	SMPositionControl _position_control; /**< class for position control calculations */
+	SMAttitudeControl _attitude_control;
+
+	uORB::SubscriptionInterval _parameter_update_sub{ORB_ID(parameter_update), 1_s};
+
+
+	//uORB::Subscription _manual_control_setpoint_sub{ORB_ID(manual_control_setpoint)};
+	uORB::SubscriptionCallbackWorkItem _vehicle_local_position_sub{this, ORB_ID(vehicle_local_position)};
+	uORB::Subscription _vehicle_local_position_setpoint_sub{ORB_ID(vehicle_local_position_setpoint)};
+	uORB::Subscription _vehicle_attitude_sub{ORB_ID(vehicle_attitude)};
+	uORB::Subscription _vehicle_angular_velocity_sub{ORB_ID(vehicle_angular_velocity)};
+	uORB::Subscription _vehicle_control_mode_sub{ORB_ID(vehicle_control_mode)};
+	uORB::Subscription _vehicle_status_sub{ORB_ID(vehicle_status)};
+
+
+
+	//uORB::Publication<vehicle_rates_setpoint_s>     _vehicle_rates_setpoint_pub{ORB_ID(vehicle_rates_setpoint)};    /**< rate setpoint publication */
+	uORB::Publication<vehicle_attitude_setpoint_s>	_vehicle_attitude_setpoint_pub{ORB_ID(vehicle_attitude_setpoint)};
+	uORB::Publication<vehicle_torque_setpoint_s>	_vehicle_torque_setpoint_pub{ORB_ID(vehicle_torque_setpoint)};
+	uORB::Publication<vehicle_thrust_setpoint_s>	_vehicle_thrust_setpoint_pub{ORB_ID(vehicle_thrust_setpoint)};
+	uORB::Publication<offboard_control_mode_s>	_offboard_control_mode_pub{ORB_ID(offboard_control_mode)};
+	// =================================================
+
+
+
+
+	//manual_control_setpoint_s       _manual_control_setpoint {};    /**< manual control setpoint */
+	vehicle_control_mode_s          _vehicle_control_mode {};       /**< vehicle control mode */
+
+	perf_counter_t  _loop_perf;             /**< loop duration performance counter */
+
+	matrix::Vector3f _thrust_setpoint_body; /**< body frame 3D thrust vector */
+
+	float _man_yaw_sp{0.f};                 /**< current yaw setpoint in manual mode */
+
+	hrt_abstime _last_run{0};
+	hrt_abstime _last_vehicle_local_position_setpoint{0};
+
+	bool _spooled_up{false}; ///< used to make sure the vehicle cannot take off during the spoolup time
+
+	DEFINE_PARAMETERS(
+		(ParamFloat<px4::params::SM_POS_LAM_X>)		_param_pos_lam_x,
+		(ParamFloat<px4::params::SM_POS_LAM_Y>)		_param_pos_lam_y,
+		(ParamFloat<px4::params::SM_POS_LAM_Z>)		_param_pos_lam_z,
+		(ParamFloat<px4::params::SM_POS_GAIN_X>)	_param_pos_gain_x,
+		(ParamFloat<px4::params::SM_POS_GAIN_Y>)	_param_pos_gain_y,
+		(ParamFloat<px4::params::SM_POS_GAIN_Z>)	_param_pos_gain_z,
+		(ParamFloat<px4::params::SM_POS_TANH>)		_param_pos_tanh_factor,
+		(ParamFloat<px4::params::SM_POS_MASS>)		_param_mass,
+		(ParamFloat<px4::params::SM_POS_T_MAX>)		_param_thrust_max,
+		(ParamFloat<px4::params::SM_ATT_LAM_X>)		_param_att_lam_x,
+		(ParamFloat<px4::params::SM_ATT_LAM_Y>)		_param_att_lam_y,
+		(ParamFloat<px4::params::SM_ATT_LAM_Z>)		_param_att_lam_z,
+		(ParamFloat<px4::params::SM_ATT_GAIN_X>)	_param_att_gain_x,
+		(ParamFloat<px4::params::SM_ATT_GAIN_Y>)	_param_att_gain_y,
+		(ParamFloat<px4::params::SM_ATT_GAIN_Z>)	_param_att_gain_z,
+		(ParamFloat<px4::params::SM_ATT_TANH>)		_param_att_tanh_factor,
+		(ParamFloat<px4::params::SM_ATT_I_XX>)		_param_inertia_xx,
+		(ParamFloat<px4::params::SM_ATT_I_YY>)		_param_inertia_yy,
+		(ParamFloat<px4::params::SM_ATT_I_ZZ>)		_param_inertia_zz
+	)
+};
+
