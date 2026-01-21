76071019efab3f3b4a5abdf595d078eb30b77b0e
k-orestis <orestiskonsta@gmail.com>
2026-01-21 18:19:01 +0100

    new nn architecture

--- src/modules/mc_neural_control/NeuralControlUnconstrained/NeuralControlUnconstrained.cpp
+++ src/modules/mc_neural_control/NeuralControlUnconstrained/NeuralControlUnconstrained.cpp
@@ -63,42 +63,94 @@ Eigen::MatrixXf openDataUnconstrained(std::string fileToOpen)
   return Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>>(matrixEntries.data(), matrixRowNumber, matrixEntries.size() / matrixRowNumber);
 }
 
+inline Eigen::VectorXf elu(const Eigen::VectorXf& x)
+{
+    return x.array().max(0.0f)
+         + (x.array().min(0.0f).exp() - 1.0f);
+}
+
+inline Eigen::VectorXf layernorm(
+    const Eigen::VectorXf& x,
+    const Eigen::VectorXf& gamma,
+    const Eigen::VectorXf& beta,
+    float eps = 1e-5f
+) {
+    const float mean = x.mean();
+    const float var = (x.array() - mean).square().mean();
+
+    Eigen::VectorXf x_hat = (x.array() - mean) / std::sqrt(var + eps);
+    return gamma.array() * x_hat.array() + beta.array();
+}
+
 NeuralControlUnconstrained::NeuralControlUnconstrained(int n_motors)
 {
   _n_motors = n_motors;
 
   try
   {
-    std::string path = "/home/model_files_unconstrained/";
+    std::string path = "/home/arl/model_files/";
+    //"/home/orestis/workspaces/torch_model_to_csv_conversion/save_model_end_to_end/build/model_files";
     PX4_INFO("loading model files");
+    _obs_mean = openDataUnconstrained(path + "running_mean_std_mean.csv");
+    _obs_var = openDataUnconstrained(path + "running_mean_std_var.csv");
+    _obs_eps = 1e-5f;
 
-    _bias_control_net_layer_1 = openDataUnconstrained(path + "bias_control_net_layer_1.csv");
-    _weight_control_net_layer_1 = openDataUnconstrained(path + "weight_control_net_layer_1.csv");
-    _bias_control_net_layer_2 = openDataUnconstrained(path + "bias_control_net_layer_2.csv");
-    _weight_control_net_layer_2 = openDataUnconstrained(path + "weight_control_net_layer_2.csv");
-    _bias_control_net_layer_3 = openDataUnconstrained(path + "bias_control_net_layer_3.csv");
-    _weight_control_net_layer_3 = openDataUnconstrained(path + "weight_control_net_layer_3.csv");
+    _bias_layer_1 = openDataUnconstrained(path + "bias_layer_1.csv");
+    _weight_layer_1 = openDataUnconstrained(path + "weight_layer_1.csv");
+    _bias_layer_2 = openDataUnconstrained(path + "bias_layer_2.csv");
+    _weight_layer_2 = openDataUnconstrained(path + "weight_layer_2.csv");
+    _bias_layer_3 = openDataUnconstrained(path + "bias_layer_3.csv");
+    _weight_layer_3 = openDataUnconstrained(path + "weight_layer_3.csv");
+    _bias_layer_4 = openDataUnconstrained(path + "bias_layer_4.csv");
+    _weight_layer_4 = openDataUnconstrained(path + "weight_layer_4.csv");
 
-    _bias_allocation_net_layer_1 = openDataUnconstrained(path + "bias_allocation_net_layer_1.csv");
-    _weight_allocation_net_layer_1 = openDataUnconstrained(path + "weight_allocation_net_layer_1.csv");
-    _bias_allocation_net_layer_2 = openDataUnconstrained(path + "bias_allocation_net_layer_2.csv");
-    _weight_allocation_net_layer_2 = openDataUnconstrained(path + "weight_allocation_net_layer_2.csv");
-    
-    _max_wrench = openDataUnconstrained(path + "max_wrench.csv");
-    _min_wrench = openDataUnconstrained(path + "min_wrench.csv");
-    _pa_rot_mat = openDataUnconstrained(path + "pa_rot_matrix.csv");
-    _pa_center = openDataUnconstrained(path + "pa_center.csv");
-    _limits_u = openDataUnconstrained(path + "lim_u.csv");
+    _bias_allocation_layer_1 = openDataUnconstrained(path + "bias_allocation_layer_1.csv");
+    _weight_allocation_layer_1 = openDataUnconstrained(path + "weight_allocation_layer_1.csv");
 
-    _min_u_training = _limits_u(0);
-    _max_u_training = _limits_u(1);
+    _bias_output_layer = openDataUnconstrained(path + "bias_output_layer.csv");
+    _weight_output_layer = openDataUnconstrained(path + "weight_output_layer.csv");
+
+    _norm_weight = openDataUnconstrained(path + "norm_weight.csv");
+    _norm_bias = openDataUnconstrained(path + "norm_bias.csv");
+
+    // _max_wrench = openDataUnconstrained(path + "max_wrench.csv");
+    // _min_wrench = openDataUnconstrained(path + "min_wrench.csv");
+    // _pa_rot_mat = openDataUnconstrained(path + "pa_rot_matrix.csv");
+    // _pa_center = openDataUnconstrained(path + "pa_center.csv");
+    // _limits_u = openDataUnconstrained(path + "lim_u.csv");
+
+    _min_u_training = _limits_u(-1);
+    _max_u_training = _limits_u(+1);
     PX4_INFO("model files loaded!");
 
     // Debug State
     _scaled_input_allocation_net = Eigen::VectorXf::Zero(6);
-    _force_clamped = Eigen::VectorXf::Zero(4);
+    _motor_cmds = Eigen::VectorXf::Zero(6);
     _force_offset_comp = Eigen::VectorXf::Zero(4);
-    _input = Eigen::VectorXf::Zero(15);
+    _input = Eigen::VectorXf::Zero(61); // 15
+
+    _static_obs = Eigen::VectorXf(48);
+    _static_obs << 1.7820054293,      1.9009206295,
+          0.5512463450,     -0.0218369160,     -0.0267387982,
+          0.1366616040,      0.8021930456,      0.7921100259,
+          0.1790838093,      0.0146056293,      0.0078212060,
+          -0.0686258152,     -3.1968226433,     -1.4655148983,
+          -0.1597105563,      0.0127784461,      0.0024540315,
+          0.0222310014,      3.4531297684,      0.3609554470,
+          0.6219188571,     -0.0135005787,      0.0072640372,
+          0.0100276815,     -0.5797182918,      4.3422651291,
+          0.1995685995,     -0.0213653855,     -0.0125859659,
+          0.1271253228,     -2.3787565231,     -5.7737803459,
+          -0.3825006187,      0.0288977493,      0.0215753485,
+          -0.2248043418,      0.0149016120,      0.0211659707,
+          0.0337398686,      0.0480504110,      0.0363027342,
+          0.0297534037,      0.0000128641,      0.0000128641,
+          0.0000128641,      0.0000128641,      0.0000128641,
+          0.0000128641;
+
+    _motor_min_thrusts = Eigen::VectorXf::Constant(_n_motors, 0.05f);
+    _motor_max_thrusts = Eigen::VectorXf::Constant(_n_motors, 1.7f);
+
 
     _frame_transf(0, 0) = 1.0f;
     _frame_transf(0, 1) = 0.0f;
@@ -119,7 +171,7 @@ NeuralControlUnconstrained::NeuralControlUnconstrained(int n_motors)
     _frame_transf_2(2, 0) = 0.0f;
     _frame_transf_2(2, 1) = 0.0f;
     _frame_transf_2(2, 2) = 1.0f;
-    
+
   }
   catch (const std::exception &e)
   {
@@ -134,7 +186,7 @@ void NeuralControlUnconstrained::fillDebugMessage(neural_control_s &message)
   message.timestamp = hrt_absolute_time();
   for (i = 0; i < _n_motors; i++)
   {
-    message.motor_thrust[i] = _force_clamped(i);
+    message.motor_thrust[i] = _motor_cmds(i); //_force_clamped(i);
   }
   for (i = 0; i < 6; i++)
   {
@@ -168,6 +220,7 @@ matrix::Vector<float,6> NeuralControlUnconstrained::updateNeural()
 
   matrix::Dcmf _attitude_local_mat = _frame_transf * (_frame_transf_2 * matrix::Dcmf(_attitude)) * _frame_transf.transpose();
   matrix::Eulerf euler_angles_local(_attitude_local_mat);
+  matrix::Quatf q_local(_attitude_local_mat); // [w,x,y,z]
 
   // PX4_INFO("________________________");
   //PX4_INFO("attitude: %f %f %f", (double)euler_angles_local.phi(), (double)euler_angles_local.theta(), (double)euler_angles_local.psi());
@@ -192,50 +245,86 @@ matrix::Vector<float,6> NeuralControlUnconstrained::updateNeural()
   vel_state << linear_velocity_local(0), linear_velocity_local(1), linear_velocity_local(2);
   Eigen::Vector3f vel_setpoint;
   vel_setpoint << linear_velocity_setpoint_local(0), linear_velocity_setpoint_local(1), linear_velocity_setpoint_local(2);
-  Eigen::Vector3f vel_input = vel_state - vel_setpoint;
+  Eigen::Vector3f vel_input = vel_setpoint - vel_state;
+  Vector3f vel_input_b = _attitude_local_mat.transpose() * vel_input;
 
-  Eigen::VectorXf attitude_state(6);
-  attitude_state << _attitude_local_mat(0, 0), _attitude_local_mat(0, 1), _attitude_local_mat(0, 2),
-      _attitude_local_mat(1, 0), _attitude_local_mat(1, 1), _attitude_local_mat(1, 2);
+  Eigen::VectorXf quat_input(4);
+  quat_input << q_local(1), q_local(2), q_local(3), q_local(0);
 
   // convert angular velocities
   Eigen::Vector3f angular_velocity_state;
   angular_velocity_state << angular_vel_local(0), angular_vel_local(1), angular_vel_local(2);
+  Vector3f angular_velocity_state_b = _attitude_local_mat.transpose() * angular_velocity_state;
 
   // input vector for network
-  _input = Eigen::VectorXf::Zero(15);
-  _input << pos_input_clamped, attitude_state, vel_input, angular_velocity_state;
+  _unnorm_input = Eigen::VectorXf::Zero(61);
+  // 0..2  position error (world)
+  _unnorm_input.segment<3>(0) = pos_input_clamped;
+
+  // 3..6  quaternion [x,y,z,w]
+  _unnorm_input.segment<4>(3) = quat_input;
+
+  // 7..9  velocity error (body)
+  _unnorm_input.segment<3>(7) = vel_input_b;
+
+  // 10..12 angular velocity error (body)
+  _unnorm_input.segment<3>(10) = angular_velocity_state_b;
+  _unnorm_input.segment<48>(13) = _static_obs;
+
+
+
+  // normalize observations
+  Eigen::VectorXf _input = _unnorm_input;
+  for (int i = 0; i < _input.size(); i++) {
+      const float denom = sqrtf(_obs_var(i) + _obs_eps);
+      float v = (_input(i) - _obs_mean(i)) / denom;
+
+      // clamp to [-5, 5]
+      if (v > 5.f) v = 5.f;
+      else if (v < -5.f) v = -5.f;
+
+      _input(i) = v;
+  }
+  Eigen::VectorXf main = _input.head(13);          // (13)
+  Eigen::VectorXf side = _input.segment(13, 48);   // (48)
 
   // forward path
-  Eigen::VectorXf co1 = _weight_control_net_layer_1 * _input + _bias_control_net_layer_1;
-  Eigen::VectorXf ca1 = co1.array().tanh();
-  Eigen::VectorXf co2 = _weight_control_net_layer_2 * ca1 + _bias_control_net_layer_2;
-  Eigen::VectorXf ca2 = co2.array().tanh();
-  Eigen::VectorXf co3 = _weight_control_net_layer_3 * ca2 + _bias_control_net_layer_3;
-  Eigen::VectorXf ca3 = co3.array().tanh();
-  Eigen::VectorXf input_allocation_net = ca3;
+  Eigen::VectorXf co1 = _weight_layer_1 * main + _bias_layer_1;
+  Eigen::VectorXf ca1 = elu(co1);
+  Eigen::VectorXf co2 = _weight_layer_2 * ca1 + _bias_layer_2;
+  Eigen::VectorXf ca2 = elu(co2);
+  Eigen::VectorXf co1_ = _weight_allocation_layer_1 * side + _bias_allocation_layer_1;
+  Eigen::VectorXf ca1_ = elu(co1_);
 
-  Eigen::VectorXf scaled_input_allocation_net = input_allocation_net.cwiseProduct(_max_wrench - _min_wrench)/2 + (_max_wrench + _min_wrench)/2;
-  Eigen::VectorXf transformed_input_allocation_net = _pa_rot_mat * scaled_input_allocation_net + _pa_center;
+  Eigen::VectorXf cat(128);
+  cat << ca2, ca1_;
+  // LAYERNORM HERE
+  cat = layernorm(cat, _norm_weight, _norm_bias);
 
-  Eigen::VectorXf ao1 = _weight_allocation_net_layer_1 * transformed_input_allocation_net + _bias_allocation_net_layer_1;
-  Eigen::VectorXf ao2 = _weight_allocation_net_layer_2 * ao1 + _bias_allocation_net_layer_2;
-  Eigen::VectorXf output_allocation_net = ao2;
+  Eigen::VectorXf co3 = _weight_layer_3 * cat + _bias_layer_3;
+  Eigen::VectorXf ca3 = elu(co3);
+  Eigen::VectorXf co4 = _weight_layer_4 * ca3 + _bias_layer_4;
+  Eigen::VectorXf ca4 = elu(co4);
+  Eigen::VectorXf output = _weight_output_layer * ca4 + _bias_output_layer;
 
+
+  Eigen::VectorXf actions = output.cwiseMax(-1.f).cwiseMin(1.f);
+  actions = (actions.array() + 1.f) * 0.5f;
   _force_clamped = Eigen::VectorXf::Zero(_n_motors);
-  _force_clamped = output_allocation_net.cwiseMax(_min_u_training).cwiseMin(_max_u_training);
+  _force_clamped =  _motor_min_thrusts + (_motor_max_thrusts - _motor_min_thrusts).cwiseProduct(actions);
+
 
   // conversion to rpm
   static const float _thrust_coefficient = 0.00001286412;
 
-  Eigen::VectorXf rps = Eigen::VectorXf::Zero(_n_motors);
-  rps = _force_clamped / _thrust_coefficient;
-  rps = rps.cwiseSqrt();
-  Eigen::VectorXf rpm = rps * 60;
+  Eigen::VectorXf rpm = Eigen::VectorXf::Zero(_n_motors);
+  rpm = _force_clamped / _thrust_coefficient;
+  rpm = rpm.cwiseSqrt();
+  //Eigen::VectorXf rpm = rps * 60;
 
   // conversion to motor commands (inverse of the scaling done in mixer module)
   matrix::Vector<float,6> motor_commands;
-
+  rpm = rpm.reverse(); // reverse for correct motor order
   if (_n_motors == 4){
     motor_commands(0) = (rpm(0) * 2 - _max_rpm - _min_rpm) / (_max_rpm - _min_rpm);
     motor_commands(1) = (rpm(2) * 2 - _max_rpm - _min_rpm) / (_max_rpm - _min_rpm);
@@ -246,6 +335,7 @@ matrix::Vector<float,6> NeuralControlUnconstrained::updateNeural()
   }
   else if (_n_motors == 6)
   {
+    // what is going on here??
     motor_commands(0) = (rpm(0) * 2 - _max_rpm - _min_rpm) / (_max_rpm - _min_rpm);
     motor_commands(1) = (rpm(2) * 2 - _max_rpm - _min_rpm) / (_max_rpm - _min_rpm);
     motor_commands(2) = (rpm(3) * 2 - _max_rpm - _min_rpm) / (_max_rpm - _min_rpm);
--- src/modules/mc_neural_control/NeuralControlUnconstrained/NeuralControlUnconstrained.hpp
+++ src/modules/mc_neural_control/NeuralControlUnconstrained/NeuralControlUnconstrained.hpp
@@ -51,7 +51,7 @@ public:
 
   Eigen::Quaternionf transform_orientation_ned_enu(Eigen::Quaternionf quat_ned);
 
-  int _n_motors; 
+  int _n_motors;
 
   /**
    * Run one control loop cycle calculation
@@ -59,17 +59,23 @@ public:
   matrix::Vector<float,6> updateNeural();
 
 private:
+  Eigen::VectorXf _norm_weight;
+  Eigen::VectorXf _norm_bias;
+  Eigen::VectorXf _bias_layer_1;
+  Eigen::MatrixXf _weight_layer_1;
+  Eigen::VectorXf _bias_layer_2;
+  Eigen::MatrixXf _weight_layer_2;
+  Eigen::VectorXf _bias_layer_3;
+  Eigen::MatrixXf _weight_layer_3;
+  Eigen::VectorXf _bias_layer_4;
+  Eigen::MatrixXf _weight_layer_4;
+  Eigen::VectorXf _bias_allocation_layer_1;
+  Eigen::MatrixXf _weight_allocation_layer_1;
+  Eigen::VectorXf _bias_output_layer;
+  Eigen::MatrixXf _weight_output_layer;
 
-  Eigen::VectorXf _bias_control_net_layer_1;
-  Eigen::MatrixXf _weight_control_net_layer_1;
-  Eigen::VectorXf _bias_control_net_layer_2;
-  Eigen::MatrixXf _weight_control_net_layer_2;
-  Eigen::VectorXf _bias_control_net_layer_3;
-  Eigen::MatrixXf _weight_control_net_layer_3;
-  Eigen::VectorXf _bias_allocation_net_layer_1;
-  Eigen::MatrixXf _weight_allocation_net_layer_1;
-  Eigen::VectorXf _bias_allocation_net_layer_2;
-  Eigen::MatrixXf _weight_allocation_net_layer_2;
+  Eigen::VectorXf _motor_min_thrusts;
+  Eigen::VectorXf _motor_max_thrusts;
   Eigen::VectorXf _max_wrench;
   Eigen::VectorXf _min_wrench;
   Eigen::MatrixXf _pa_rot_mat;
@@ -87,7 +93,7 @@ private:
   matrix::Dcmf _frame_transf;
   matrix::Dcmf _frame_transf_2;
 
-  // this are the min and max forces that the motor 
+  // this are the min and max forces that the motor
   // can generate and have to be estimated from the real sysneural_control_stem
 
   // setpoints
@@ -103,8 +109,14 @@ private:
   // Debug States
   Eigen::VectorXf _scaled_input_allocation_net;
   Eigen::VectorXf _force_clamped;
+  Eigen::VectorXf _motor_cmds;
   Eigen::VectorXf _force_offset_comp;
   Eigen::VectorXf _input;
   matrix::Vector<float,6> _debug;
+
+
+  Eigen::VectorXf _obs_mean;  // (61)
+  Eigen::VectorXf _obs_var;   // (61)
+  float _obs_eps{1e-5f};
 
 };
